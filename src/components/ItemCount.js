28338498f7f53e2a70c787d81bacef309915bbe3
Lucas Poma <81720662+pomalucas@users.noreply.github.com>
2022-03-07 07:57:18 -0300

    Cree nuevo componente contador

---
+++ src/components/ItemCount.js
@@ -0,0 +1,31 @@
+import {useState} from "react"
+
+const ItemCount = (props) => {
+    const [contador, setContador] = useState(props.initial)
+
+    const stock = 2;
+
+    const handleClick = () => {
+        setContador(contador + 1)
+    }
+
+    const restar = () => {
+        setContador(contador - 1)
+    }
+
+    const resetear = () => {
+        setContador(0)
+    }
+
+    return (
+        <div>
+            <p className="count">Mi stock es {stock}</p>
+            <p>Mi contador actual : {contador}</p>
+            <button onClick={handleClick}>aumentar</button>
+            <button onClick={resetear}>resetear</button>
+            <button onClick={restar}>restar</button>
+        </div>
+    );
+}
+
+export default ItemCount
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,7 +1,16 @@
+import React from 'react';
+import ItemCount from './ItemCount';
+
 const ItemListContainer = (props) => {
+
+    const onAdd = () => {
+        console.log("Agregado al carrito");
+    }
+
     return (
         <div>
             <h2>{props.greeting}</h2>
+            <ItemCount initial={1} stock={5} onAdd={onAdd} />
         </div>
     )
 }
